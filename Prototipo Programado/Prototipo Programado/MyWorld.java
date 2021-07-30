d2a96840362672993cb08bc5a77f7a1facce8874
JorgeAndrino21616 <and21616@uvg.edu.gt>
2021-07-30 15:07:20 -0600

    spawn aleatorio de objetos

--- Prototipo Programado/Prototipo Programado/MyWorld.java
+++ Prototipo Programado/Prototipo Programado/MyWorld.java
@@ -18,8 +18,29 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         addObject (new Oso(), 300, 200);
-        addObject (new Cereza(), 100, 100);
-        addObject (new Naranja(), 300, 300);
-        addObject (new obstaculo(), 400, 250);
+        Cereza[] cerezas = new Cereza[7];
+        for(int i=0; i<cerezas.length; i++)
+        {
+            cerezas[i]= new Cereza();
+            int cerezaX = Greenfoot.getRandomNumber(getWidth());
+            int cerezaY = Greenfoot.getRandomNumber(getHeight());
+            addObject(cerezas[i], cerezaX, cerezaY);
+        }
+        Naranja[] naranjas = new Naranja[7];
+        for(int i=0; i<naranjas.length; i++)
+        {
+            naranjas[i]= new Naranja();
+            int naranjaX = Greenfoot.getRandomNumber(getWidth());
+            int naranjaY = Greenfoot.getRandomNumber(getHeight());
+            addObject(naranjas[i], naranjaX, naranjaY);
+        }
+        obstaculo[] obstaculos = new obstaculo[6];
+        for(int i=0; i<obstaculos.length; i++)
+        {
+            obstaculos[i]= new obstaculo();
+            int obstaculoX = Greenfoot.getRandomNumber(getWidth());
+            int obstaculoY = Greenfoot.getRandomNumber(getHeight());
+            addObject(obstaculos[i], obstaculoX, obstaculoY);
+        }
     }
 }
